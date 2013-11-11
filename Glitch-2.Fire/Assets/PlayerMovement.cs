c3f462cb125c7fb539c8dc805eb1fe7d53286a9b
Kevin Wong <kevinjwo@usc.edu>
2013-11-10 20:49:06 -0800

    Working on adding the powerups

--- Glitch-2.Fire/Assets/PlayerMovement.cs
+++ Glitch-2.Fire/Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	
 	public float jumpSpeed = 50.0f;
-	public float timeBetweenJumps = 0.5f; 
+	public float timeBetweenJumps = 1.0f; 
 	
 	/*
 	public float myYPosition; 
@@ -32,7 +32,7 @@ public class PlayerMovement : MonoBehaviour {
 	IEnumerator Jump(){
 		isJumping = true; 
 		rigidbody.AddForce(Vector3.up *jumpSpeed);
-		yield return new WaitForSeconds(1.0f); 
+		yield return new WaitForSeconds(timeBetweenJumps); 
 		isJumping = false; 
 		
 //		myYPosition = ((float) Transform.position.y); 
@@ -43,7 +43,42 @@ public class PlayerMovement : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider otherCollider){
 		
-		jumpSpeed = 0.0f;
+		if(otherCollider.gameObject.name("DoubleJump"))
+		{
+			enabledDoubleJump(); 	
+		}
+		else if(otherCollider.gameObject.name("Boost"))
+		{
+			enableBoost(); 
+		}
+		else if(otherCollider.gameObject.name("Win"))
+		{
+			enableWin();	
+		}
+		else if(otherCollider.gameObject.name("Save"))
+		{
+			
+		}
 		
+	}
+	
+	void enableDoubleJump()
+	{
+		timeBetweenJumps = 0.5f; 
+		yield return new WaitForSeconds(5.0f); 
+		timeBetweenJumps = 1.0f; 
+	}
+	
+	void enableBoost()
+	{
+		jumpSpeed = 75.0f;
+		yield return new WaitForSeconds(5.0f); 
+		jumpSpeed = 50.0f; 
+	}
+	
+	void enableWin()
+	{
+		Time.timeScale = 0.0f; 
+		/
 	}
 }
---
+++ Glitch-2.Fire/Assets/powerup_dblJump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class powerup_dblJump : MonoBehaviour {
+	PlayerMovement player; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider otherCollider) 
+	{
+		if(otherCollider.gameObject.name.Contains("Player"))
+		{
+			player.enableDoubleJump(); 
+		}
+	}
+}
