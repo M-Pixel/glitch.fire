dd14009afcf44fff4f78306a9a2dcc7d557aeed6
Kevin Wong <kevinjwo@usc.edu>
2013-11-12 15:06:15 -0800

    upgraded to 4.3

--- Glitch-2.Fire/Assets/PlayerMovement.cs
+++ Glitch-2.Fire/Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	
 	public float jumpSpeed = 50.0f;
-	public float timeBetweenJumps = 1.0f; 
+	public float timeBetweenJumps = 0.5f; 
 	
 	/*
 	public float myYPosition; 
@@ -32,7 +32,7 @@ public class PlayerMovement : MonoBehaviour {
 	IEnumerator Jump(){
 		isJumping = true; 
 		rigidbody.AddForce(Vector3.up *jumpSpeed);
-		yield return new WaitForSeconds(timeBetweenJumps); 
+		yield return new WaitForSeconds(1.0f); 
 		isJumping = false; 
 		
 //		myYPosition = ((float) Transform.position.y); 
@@ -42,43 +42,12 @@ public class PlayerMovement : MonoBehaviour {
 
 	
 	void OnTriggerEnter(Collider otherCollider){
+		/*
+		if(otherCollider.GetComponent.name.equals("doubleJump")
+		  {
+
+		}
+*/
 		
-		if(otherCollider.gameObject.name("DoubleJump"))
-		{
-			enabledDoubleJump(); 	
-		}
-		else if(otherCollider.gameObject.name("Boost"))
-		{
-			enableBoost(); 
-		}
-		else if(otherCollider.gameObject.name("Win"))
-		{
-			enableWin();	
-		}
-		else if(otherCollider.gameObject.name("Save"))
-		{
-			
-		}
-		
-	}
-	
-	void enableDoubleJump()
-	{
-		timeBetweenJumps = 0.5f; 
-		yield return new WaitForSeconds(5.0f); 
-		timeBetweenJumps = 1.0f; 
-	}
-	
-	void enableBoost()
-	{
-		jumpSpeed = 75.0f;
-		yield return new WaitForSeconds(5.0f); 
-		jumpSpeed = 50.0f; 
-	}
-	
-	void enableWin()
-	{
-		Time.timeScale = 0.0f; 
-		/
 	}
 }
