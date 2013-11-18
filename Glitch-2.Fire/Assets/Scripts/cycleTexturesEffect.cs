80522b8afd6958176c3040124548e0b8ea2e3970
Kevin Wong <kevinjwo@usc.edu>
2013-11-17 23:32:57 -0800

    added teleporter to Level2

--- Glitch-2.Fire/Assets/Scripts/cycleTexturesEffect.cs
+++ Glitch-2.Fire/Assets/Scripts/cycleTexturesEffect.cs
@@ -18,10 +18,11 @@ public class cycleTexturesEffect : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		//yield WaitForSeconds(3.0f); 
 		//Make sure the textures have been assigned. Unity will often give you a nice
 		//clear error message if you've forgotten to assign a value to something in a
 		//script, but in this case it just complains about dividing by zero (see below)
-		if (textures.Length > 0) {
+		/*if (textures.Length > 0) {
 			//the arguments here are the function name, how long to wait before starting,
 			//how long to wait between calls
 			InvokeRepeating("CycleTextures", changeInterval, changeInterval);
@@ -35,13 +36,23 @@ public class cycleTexturesEffect : MonoBehaviour {
 		} else {
 			Debug.LogWarning ("No sounds specified for gameObject "+gameObject.name+"!");
 		}
+		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		StartCoroutine(CycleTexture());
 	
 	}
 
+	IEnumerator CycleTexture()
+	{
+		yield return new WaitForSeconds(3.0f); 
+		currentTextureIndex = ++currentTextureIndex % textures.Length;
+		guiTexture.texture = textures[currentTextureIndex];
+		yield return new WaitForSeconds(3.0f); 
+	}
+	/*
 	void CycleTextures () {
 		//this is where the division by zero happens. The % is the modulo
 		//operator, which means "divide by this and get the remainder".
@@ -51,10 +62,12 @@ public class cycleTexturesEffect : MonoBehaviour {
 		//These lines cause the object to show a series of textures (the ones
 		//you chose in the editor) in sequence, over and over. See the end of
 		//the Player class for more detail.
+	//	
 		currentTextureIndex = ++currentTextureIndex % textures.Length;
 		guiTexture.texture = textures[currentTextureIndex];
 		
 	}
+	*/
 
 
 }
---
+++ Glitch-2.Fire/Assets/teleportToNewLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleportToNewLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		StartCoroutine(teleportPlayer()); 
+	}
+
+	IEnumerator teleportPlayer()
+	{
+		yield return new WaitForSeconds(3.0f); 
+		Application.LoadLevel(levelTwo); 
+	}
+}
