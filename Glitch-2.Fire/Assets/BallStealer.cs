113c623422eb5e19985c2da96176041627bd7a6d
Max Pittsley (tablet) <max.pittsley@gmail.com>
2013-11-20 17:28:57 -0800

    Ball stays with platforms it lands on (sometimes)
    Still needs tweaking...

---
+++ Glitch-2.Fire/Assets/BallStealer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallStealer : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter (Collider otherCollider) {
+		if (otherCollider.gameObject.name.Contains ("PlayerModel")) {
+			Debug.Log("colliding with player");
+			otherCollider.transform.parent.parent = transform;
+		}
+	}
+}
